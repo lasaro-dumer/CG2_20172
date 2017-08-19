89265b030476d6b075368999864eb8f66e7e1917
Lásaro Dumer <ladumer@hotmail.com>
2017-08-18 23:58:04 -0300

    'Project' template and creation

---
+++ baseProj/MainFile.cpp
@@ -0,0 +1,172 @@
+
+#include <iostream>
+#include <cmath>
+#include "SOIL/SOIL.h"
+#include "ImageClass.hpp"
+
+using namespace std;
+ImageClass Image;
+ImageClass NewImage;
+const int LIMIAR = 120;
+#define LARGURA_JAN 1000
+#define ALTURA_JAN 500
+// **********************************************************************
+//  void init(void)
+// **********************************************************************
+void init()
+{
+    int r;
+    // Carrega a uma image
+    string nome = "imgs/Falcao.jpg";
+
+    string path = "";
+
+    nome =  path + nome;
+    cout << "imagem a ser carregada: *" << nome << "*" << endl;
+    r = Image.Load(nome.c_str()); // Carrega uma imagem
+
+
+    if (!r) exit(1); // Erro na carga da imagem
+    else cout << ("Imagem carregada!\n");
+
+    // Ajusta o tamnho da imagem da direita, para que ela
+    // passe a ter o mesmo tamnho da imagem recem carregada
+    // Caso precise alterar o tamanho da nova imagem, mude os parâmetros
+    // da na chamada abaixo
+    NewImage.SetSize(Image.SizeX(), Image.SizeY(), Image.Channels());
+    cout << "Nova Imagem Criada" << endl;
+
+}
+// **********************************************************************
+//  void reshape( int w, int h )
+//  trata o redimensionamento da janela OpenGL
+// **********************************************************************
+void reshape( int w, int h )
+{
+
+    // Reset the coordinate system before modifying
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+    gluOrtho2D(0,w,0,h);
+
+    // Set the clipping volume
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+}
+// **********************************************************************
+//  void display( void )
+// **********************************************************************
+void display( void )
+{
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Fundo de tela preto
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+
+// Ajusta o ZOOM da imagem para que apareca na metade da janela
+    float zoomH = (glutGet(GLUT_WINDOW_WIDTH)/2.0)/(double)Image.SizeX();
+    float zoomV = (glutGet(GLUT_WINDOW_HEIGHT))/(double)Image.SizeY();
+    Image.SetZoomH(zoomH);
+    Image.SetZoomV(zoomV);
+// posiciona a imagem no canto inferior esquerdo da janela
+    Image.SetPos(0, 0);
+
+// posiciona a imagem nova na metada da direita da janela
+    NewImage.SetPos(glutGet(GLUT_WINDOW_WIDTH)/2, 0);
+
+// Ajusta o ZOOM da imagem para que apareca na metade da janela
+    NewImage.SetZoomH(zoomH);
+    NewImage.SetZoomV(zoomV);
+
+// Coloca as imagens na tela
+    Image.Display();
+    NewImage.Display();
+
+// Mostra a tela OpenGL
+    glutSwapBuffers();
+}
+// **********************************************************************
+//  void keyboard ( unsigned char key, int x, int y )
+// **********************************************************************
+void keyboard ( unsigned char key, int x, int y )
+{
+
+    switch ( key )
+    {
+    case 27: // Termina o programa qdo
+        NewImage.Delete();
+        Image.Delete();
+        exit ( 0 );   // a tecla ESC for pressionada
+        break;
+    case '2':
+        //ConvertBlackAndWhite();
+        glutPostRedisplay();    // obrigatório para redesenhar a tela
+        break;
+
+    case 'g':
+        //ConvertToGrayscale();
+        glutPostRedisplay();    // obrigatório para redesenhar a tela
+        break;
+
+    case 'b':
+        //DetectImageBorders();
+        glutPostRedisplay();    // obrigatório para redesenhar a tela
+        break;
+    case 'i':
+        //InvertImage();
+        glutPostRedisplay();    // obrigatório para redesenhar a tela
+        break;
+     case 'm':
+        //Mediana();
+        glutPostRedisplay();    // obrigatório para redesenhar a tela
+        break;
+    default:
+        break;
+    }
+}
+
+// **********************************************************************
+//  void arrow_keys ( int a_keys, int x, int y )
+// **********************************************************************
+void arrow_keys ( int a_keys, int x, int y )
+{
+    switch ( a_keys )
+    {
+    case GLUT_KEY_UP:       // When Up Arrow Is Pressed...
+        break;
+    case GLUT_KEY_DOWN:     // When Down Arrow Is Pressed...
+
+        break;
+    default:
+        break;
+    }
+}
+// **********************************************************************
+//  void main ( int argc, char** argv )
+// **********************************************************************
+
+int main ( int argc, char** argv )
+{
+    glutInit            ( &argc, argv );
+
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB );
+    glutInitWindowPosition (10,10);
+
+    // Define o tamanho da janela gráfica do programa
+    glutInitWindowSize  ( LARGURA_JAN, ALTURA_JAN);
+    glutCreateWindow    ( "Image Loader" );
+
+    init ();
+
+    glutDisplayFunc ( display );
+    glutReshapeFunc ( reshape );
+    glutKeyboardFunc ( keyboard );
+    glutSpecialFunc ( arrow_keys );
+    //glutIdleFunc ( display );
+
+    glutMainLoop ( );
+    return 0;
+}
---
+++ mkProj.sh
@@ -0,0 +1,8 @@
+echo "Creating project" $1
+rm -rf $1
+mkdir -p $1
+ln -s ../ImageClass/SOIL $1
+cp ImageClass/ImageClass.cpp $1/ImageClass.cpp
+cp ImageClass/ImageClass.hpp $1/ImageClass.hpp
+cp baseProj/MainFile.cpp $1/MainFile.cpp
+cp baseProj/Makefile $1/Makefile
